c5fac877c6f1deb19fa2fee07a376d95e4ebdcf9
Chirag Rajput <chirag.3r@gmail.com>
2024-04-26 19:07:49 +0530

    all events navbar and sidebar structured

--- frontend/src/components/audience/Events/SponsoredEventsSidebar.jsx
+++ frontend/src/components/audience/Events/SponsoredEventsSidebar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const SponsoredEventsSidebar = () => {
   return (
-    <div className='bg-black min-h-screen'>
-      sidebar
+    <div className="bg-black min-h-screen flex flex-col gap-2  p-2">
+      <p className="text-white">Promoted Events</p>
+      <div className="bg-white p-2 rounded-md">
+        <img
+          src="https://via.placeholder.com/150"
+          alt="placeholder"
+          className="w-full rounded-md"
+        />
+        <p className="">Event Name</p>
+      </div>
+      <div className="bg-white p-2 rounded-md">
+        <img
+          src="https://via.placeholder.com/150"
+          alt="placeholder"
+          className="w-full rounded-md"
+        />
+        <p className="">Event Name</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SponsoredEventsSidebar
+export default SponsoredEventsSidebar;
